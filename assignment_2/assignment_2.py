bc4c4c3703afe77dc95219f6a97ae282778f6148
Vinicius Couto Espindola <vinicius.c.e@gmail.com>
2020-04-16 07:49:22 -0300

    Fixed a bunch of things

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from scipy import ndimage
 from scipy import misc
-import sys, getopt
+import sys, argparse
 
 # h1 - ?????????????
 h1_ukn = np.array([[ 0,  0, -1,  0,  0],
@@ -19,7 +19,7 @@ h2_gauss = np.array([[1,  4,  6,  4, 1],
                      [1,  4,  6,  4, 1]])/256
 
 # h3 - gradient detection (embossing) WEST
-h3_west_emboss = np.array([[-1, 0, 1],
+h3_east_emboss = np.array([[-1, 0, 1],
                            [-2, 0, 2],
                            [-1, 0, 1]])
 
@@ -54,38 +54,54 @@ h8_left_diag = np.array([[ 2, -1, -1],
 # cv2.waitKey()
 
 def main(argv):
-    try:
-        input_path  = argv[1]
+    parser = argparse.ArgumentParser(
+        description='''Apply selected filter to input image. 
+        If more than one defined, sums their individual outputs.'''
+        )
+    parser.add_argument('input', help='Path of the input image.')
+    parser.add_argument('ker', type=int, choices=list(range(1,9)),
+                        help='ID (x) of the kernel (Hx) to be used.'
+                        )
+    parser.add_argument('--sum', type=int, choices=list(range(1,9)), 
+                        help='Takes a second filter and sum the results with: sqrt(ker**2 + pit**2)'
+                        )
+    parser.add_argument('-o','--output', 
+                        help='Path where the output image will be saved.'
+                        )
 
-        print(argv)
-        ker = {
-            '1':h1_ukn, 
-            '2':h2_gauss,
-            '3':h3_west_emboss,
-            '4':h4_north_emboss,
-            '5':h5_high_pass,
-            '6':h6_arith_mean,
-            '7':h7_right_diag,
-            '8':h8_left_diag
-        }.get(argv[2])
+    args = parser.parse_args()
 
-    except:
-        print(f'''
-        Script Usage:
-        \t{argv[0]} <function> <functionargs> <inputfilepath>'
+    kernels = {
+        1:h1_ukn,
+        2:h2_gauss,
+        3:h3_east_emboss,
+        4:h4_north_emboss,
+        5:h5_high_pass,
+        6:h6_arith_mean,
+        7:h7_right_diag,
+        8:h8_left_diag
+    }
 
-        Make sure the command in syntatically correct.
-         - Function argument must match function name
-         - Arguments must be separated using commas => arg1,arg2,arg3
-         - A existing input path must be given
-         ''')
-        sys.exit(2)
+    print(f"Loading image {args.input} ...")
+    img = cv2.imread(args.input, cv2.IMREAD_GRAYSCALE)
+    ker = cv2.flip(kernels[args.ker], -1)
+    out = cv2.filter2D(img, -1, ker)
+    
+    print(out)
 
-    print(f"Loading image {input_path} ...")
-    img = cv2.imread(input_path, cv2.IMREAD_GRAYSCALE)
-    test = cv2.filter2D(img, -1, ker)
-    cv2.imshow("test",test.astype(np.uint8))
-    cv2.waitKey()
+    if args.sum : 
+        ker = cv2.flip(kernels[args.sum], -1)
+        out1 = out.astype(np.float32)
+        out2 = cv2.filter2D(img, -1, ker).astype(np.float32)
+        out = np.sqrt(out1**2 + out2**2)
+        out[out>255] = 255
+        out = out.astype(np.uint8)
+ 
+    if not args.output:
+        cv2.imshow("test", out)
+        cv2.waitKey()
+    else:
+        cv2.imwrite(args.output, out)
     
 
 if __name__ == "__main__":
